98a734d69d8b44769787841d57f516631ff07627
UnicornDad8 <unicorndad8@gmail.com>
2023-10-11 14:37:28 -0300

    Finalizing Splash screen

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import Splash from "./src/screens/Splash";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return <Splash />;
--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, ImageBackground, Image } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import Button from "../../components/Button/Button";
 import colors from "../../constants/Colors";
 import styles from "./Splash.modules.css";
@@ -7,19 +8,30 @@ import styles from "./Splash.modules.css";
 const Splash = () => {
   return (
     <View>
+      <StatusBar style="light" />
       <ImageBackground
         style={{ width: "100%", height: "100%" }}
         source={require("../../../assets/splash.png")}
       >
         <View style={styles.container}>
-          <Image
-            style={styles.logo}
-            source={require("../../../assets/logo.png")}
-          />
-          <Text style={[styles.title, { color: colors.white }]}>
-            100K+ Premium Recipe
-          </Text>
-          <Button>Start Cooking</Button>
+          <View>
+            <Image
+              style={styles.logo}
+              source={require("../../../assets/logo.png")}
+            />
+            <Text style={[styles.title, { color: colors.white }]}>
+              100K+ Premium Recipe
+            </Text>
+          </View>
+          <View>
+            <Text style={[styles.bigTitle, { color: colors.white }]}>
+              Get Cooking
+            </Text>
+            <Text style={[styles.subtitle, { color: colors.white }]}>
+              Simple way to find Tasty Recipe
+            </Text>
+            <Button>Start Cooking</Button>
+          </View>
         </View>
       </ImageBackground>
     </View>
